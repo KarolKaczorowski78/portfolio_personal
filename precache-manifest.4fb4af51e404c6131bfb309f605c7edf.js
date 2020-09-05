96187ff8c897d7fdf05e00980881d664565828f9
Karol <KarolKaczorowski@interia.pl>
2020-09-05 18:36:05 +0200

    Updates

---
+++ precache-manifest.4fb4af51e404c6131bfb309f605c7edf.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e494c167164081daf4240069562920d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9de3277f1220a5a54a4",
+    "url": "/static/css/main.9c78fd67.chunk.css"
+  },
+  {
+    "revision": "0eda02235ebedd77b1a3",
+    "url": "/static/js/2.b24e28d9.chunk.js"
+  },
+  {
+    "revision": "d668461d5db0cc796abe8fa898597524",
+    "url": "/static/js/2.b24e28d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9de3277f1220a5a54a4",
+    "url": "/static/js/main.24b9506a.chunk.js"
+  },
+  {
+    "revision": "2d21f2ec06dea5ffe7e7",
+    "url": "/static/js/runtime-main.ace9268a.js"
+  },
+  {
+    "revision": "a91141be4018f1d939b3f8202f9b0d19",
+    "url": "/static/media/DrinksMain.a91141be.jpg"
+  },
+  {
+    "revision": "2217b1163d325a6606f9b9a6803a180a",
+    "url": "/static/media/PuzzleSolve.2217b116.jpg"
+  },
+  {
+    "revision": "93b136ffa66e799360420a2f5c926762",
+    "url": "/static/media/about.93b136ff.jpg"
+  },
+  {
+    "revision": "b6a25e4d66096edc4f12bc130c7466b4",
+    "url": "/static/media/babel.b6a25e4d.svg"
+  },
+  {
+    "revision": "889449e31190719bac49e10699663581",
+    "url": "/static/media/css.889449e3.svg"
+  },
+  {
+    "revision": "d196a697731774bf330a956ffd92cbb9",
+    "url": "/static/media/eComDetails.d196a697.jpg"
+  },
+  {
+    "revision": "f6050941083c77b1ec0b6753a1456dfa",
+    "url": "/static/media/git.f6050941.svg"
+  },
+  {
+    "revision": "5e025b389b303c0d010a3294eb183d21",
+    "url": "/static/media/html.5e025b38.svg"
+  },
+  {
+    "revision": "ecf1aec98b3724e4d65ebc65be05e107",
+    "url": "/static/media/js.ecf1aec9.svg"
+  },
+  {
+    "revision": "1f6a76a4bf66ee1d6bdeab1edc5e2b5c",
+    "url": "/static/media/react.1f6a76a4.svg"
+  },
+  {
+    "revision": "cb1608003ac33cc02f3793c85370a97d",
+    "url": "/static/media/sass.cb160800.svg"
+  },
+  {
+    "revision": "70e5b2e8ee733eeffc7ce553dd85af03",
+    "url": "/static/media/ts.70e5b2e8.svg"
+  },
+  {
+    "revision": "e4378edb89c7ac91be6f80665a801d42",
+    "url": "/static/media/webpack.e4378edb.svg"
+  }
+]);
